2c7b0d9d6c6dedca0c4715f69c64d0656a5e5cb6
CAT <14349453+ihexon@users.noreply.github.com>
2025-09-13 01:58:43 +0800

    feat(restapi): remove local exec api, just /exec (#48)

--- pkg/server/exec.go
+++ pkg/server/exec.go
@@ -1,56 +1,38 @@
 package server
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"linuxvm/pkg/network"
 	"linuxvm/pkg/ssh"
 	"linuxvm/pkg/vmconfig"
+	"net/http"
+	"sync"
 
-	"os/exec"
-
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
-	"github.com/tmaxmax/go-sse"
 )
 
-type ExecAction struct {
+type execAction struct {
 	Bin  string   `json:"bin,omitempty"`
 	Args []string `json:"args,omitempty"`
 	Env  []string `json:"env,omitempty"`
 }
 
-const (
-	TypeOut = "out"
-	TypeErr = "error"
-)
-
-func createSSEMsgAndPublish(msgType, msg string, sseServer *sse.Server, topic string) {
-	m := &sse.Message{}
-	m.AppendData(msg)
-	m.Type = sse.Type(msgType)
-	if err := sseServer.Publish(m, topic); err != nil {
-		logrus.Warnf("Error publishing message: %v", err)
-	}
-}
-
-type ExecInfo struct {
-	outPip   io.Reader
-	errPip   io.Reader
-	process  *exec.Cmd
+type ExecProcess struct {
+	outPipe  io.Reader
+	errPipe  io.Reader
 	exitChan chan error
 }
 
-const (
-	topicKey = "topicKey"
-)
+func GuestExec(ctx context.Context, vmc *vmconfig.VMConfig, bin string, args ...string) (*ExecProcess, error) {
+	process := &ExecProcess{
+		exitChan: make(chan error, 1),
+	}
 
-var (
-	sseServer *sse.Server
-)
-
-func GuestExec(ctx context.Context, vmc *vmconfig.VMConfig, bin string, args ...string) (*ExecInfo, error) {
-	execInfo := &ExecInfo{}
 	addr, err := network.ParseUnixAddr(vmc.GVproxyEndpoint)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse gvproxy endpoint: %w", err)
@@ -58,51 +40,103 @@ func GuestExec(ctx context.Context, vmc *vmconfig.VMConfig, bin string, args ...
 
 	cfg := ssh.NewCfg(vmc.SSHInfo.GuestAddr, vmc.SSHInfo.User, vmc.SSHInfo.Port, vmc.SSHInfo.HostSSHKeyPairFile)
 	defer cfg.CleanUp.CleanIfErr(&err)
+
 	cfg.SetPty(false)
 	cfg.SetCmdLine(bin, args)
-	err = cfg.Connect(ctx, addr.Path)
-	if err != nil {
+
+	if err = cfg.Connect(ctx, addr.Path); err != nil {
 		return nil, fmt.Errorf("failed to connect to gvproxy: %w", err)
 	}
+
 	if err = cfg.MakeStdPipe(); err != nil {
 		return nil, fmt.Errorf("failed to make std pipe: %w", err)
 	}
-	execInfo.errPip = cfg.Stderr
-	execInfo.outPip = cfg.Stdout
+
+	process.errPipe = cfg.Stderr
+	process.outPipe = cfg.Stdout
 
 	go func() {
-		execInfo.exitChan <- cfg.Run(ctx)
+		err := cfg.Run(ctx)
+		defer cfg.CleanUp.CleanIfErr(&err)
+		process.exitChan <- err
 	}()
 
-	return nil, nil
+	return process, nil
 }
 
-func LocalExec(ctx context.Context, bin string, args ...string) (*ExecInfo, error) {
-	cmd := exec.CommandContext(ctx, bin, args...)
-
-	outPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
+func (s *Server) doExec(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
-	errPipe, err := cmd.StderrPipe()
-	if err != nil {
-		return nil, err
+	if sseServer == nil {
+		sseServer = newSSEServer()
 	}
 
-	if err = cmd.Start(); err != nil {
-		return nil, err
+	var action execAction
+	if err := json.NewDecoder(r.Body).Decode(&action); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("invalid json"))
+		return
 	}
-	execInfo := &ExecInfo{
-		outPip:   outPipe,
-		errPip:   errPipe,
-		process:  cmd,
-		exitChan: make(chan error, 1),
-	}
+
+	ctx, cancel := context.WithCancel(context.WithValue(r.Context(), topicKey, "sess-"+uuid.NewString())) //nolint:staticcheck
+	defer cancel()
 
 	go func() {
-		execInfo.exitChan <- execInfo.process.Wait()
+		var (
+			process *ExecProcess
+			err     error
+		)
+
+		defer cancel()
+
+		topic, ok := ctx.Value(topicKey).(string)
+		if !ok {
+			logrus.Warn("empty topic in go func")
+			return
+		}
+
+		if r.URL.Path == guestexecURL {
+			process, err = GuestExec(ctx, s.Vmc, action.Bin, action.Args...)
+			if err != nil {
+				createSSEMsgAndPublish(TypeErr, "guest exec failed: "+err.Error(), sseServer, topic)
+				return
+			}
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			sc := bufio.NewScanner(process.outPipe)
+			sc.Buffer(make([]byte, 64*1024), 1<<20) // 1MB
+			for sc.Scan() {
+				createSSEMsgAndPublish(TypeOut, sc.Text(), sseServer, topic)
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			sc := bufio.NewScanner(process.errPipe)
+			sc.Buffer(make([]byte, 64*1024), 1<<20) // 1MB
+			for sc.Scan() {
+				createSSEMsgAndPublish(TypeErr, sc.Text(), sseServer, topic)
+			}
+		}()
+
+		wg.Wait()
+
+		if err = <-process.exitChan; err != nil {
+			logrus.Debugf("process exit with %v", err)
+			createSSEMsgAndPublish(TypeErr, "wait: "+err.Error(), sseServer, topic)
+			return
+		}
+
+		createSSEMsgAndPublish("done", "done", sseServer, topic)
 	}()
 
-	return execInfo, nil
+	sseServer.ServeHTTP(w, r.WithContext(ctx))
 }
