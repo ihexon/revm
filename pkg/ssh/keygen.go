c576c46ac14d9b5d95b2901d6ee8751fcbb90e80
CAT <14349453+ihexon@users.noreply.github.com>
2025-09-19 00:37:09 +0800

    refactor: separation of responsibilities to create vmprovider (#62)

--- pkg/ssh/keygen.go
+++ pkg/ssh/keygen.go
@@ -5,29 +5,26 @@ import (
 	"path/filepath"
 
 	"github.com/charmbracelet/keygen"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 )
 
 // GenerateHostSSHKeyPair generates an SSH key pair for the host and writes it to the specified destination directory.
 func GenerateHostSSHKeyPair(dest string) (*keygen.KeyPair, error) {
 	if dest == "" {
-		return nil, errors.New("function parameter error, dest is empty")
+		return nil, fmt.Errorf("function parameter error, dest is empty")
 	}
 
 	dest, err := filepath.Abs(dest)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get absolute path: %w", err)
+		return nil, err
 	}
 
-	logrus.Infof("ssh keypair will be generate in %q with keytype %q", dest, keygen.Ed25519)
 	k, err := keygen.New(
 		dest,
 		keygen.WithKeyType(keygen.Ed25519),
 	)
 
 	if err != nil {
-		return nil, fmt.Errorf("new generates a KeyPair with error : %w", err)
+		return nil, fmt.Errorf("keygen.New error: %w", err)
 	}
 
 	return k, k.WriteKeys()
